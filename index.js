3da9ab234afe88e5f60651cb8f3dfadbb914bb2a
risonakamo <hmykanae@gmail.com>
2017-12-13 20:20:12 -0500

    translate api data into something filtered

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function main()
 {
     var username="risona";
 
-    alistReq(`{MediaListCollection(userName:"${username}",type:ANIME){statusLists{media{title{romaji},startDate{year},season,coverImage{large}}},customLists{media{title{romaji},startDate{year},season,coverImage{large}}}}}`,
+    alistReq(`{MediaListCollection(userName:"${username}",type:ANIME){statusLists{media{title{romaji},startDate{year,month,day},season,coverImage{large},genres}},customLists{media{title{romaji},startDate{year,month,day},season,coverImage{large},genres}}}}`,
     (d)=>{
-        console.log(d);
+        console.log(seasonYearFilter(d,2018,"WINTER"));
     });
 }
 
@@ -24,4 +24,35 @@ function alistReq(query,callback)
 
     r.setRequestHeader("content-type","application/json");
     r.send(JSON.stringify({query:query}));
+}
+
+//give it direct data from alistreq
+function seasonYearFilter(data,year,season)
+{
+    data=data.data.MediaListCollection;
+    var res=[];
+    var seen=new Set();
+
+    //speed
+    var currentArray;
+    var currentShow;
+
+    for (var x in data)
+    {
+        for (var y in data[x])
+        {
+            currentArray=data[x][y];
+            for (var z=0,l=currentArray.length;z<l;z++)
+            {
+                currentShow=currentArray[z].media;
+                if (currentShow.season==season && currentShow.startDate.year==year && !seen.has(currentShow.title.romaji))
+                {
+                    seen.add(currentShow.title.romaji);
+                    res.push(currentShow);
+                }
+            }
+        }
+    }
+
+    return res;
 }
